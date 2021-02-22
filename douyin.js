08ab236fa46423a166c79168b74fe2e6aa34a8fb
huzing2524 <39394831+huzing2524@users.noreply.github.com>
2021-02-22 13:43:22 +0800

    Add files via upload

---
+++ douyin.js
@@ -0,0 +1,20 @@
+auto();
+var appName=rawInput("请输入app的名称");
+launchApp(appName);
+sleep(1000);
+setScreenMetrics(1080, 1920);
+
+sleep(1000);
+
+function getRandom (n, m) {
+    var num = Math.floor(Math.random() * (m - n + 1) + n)
+    return num
+}
+
+var randomTime = getRandom(5, 15) * 1000  // 视频观看时间：5~15秒
+var num = 200;
+
+while(num > 1){
+    swipe(device.width / 2, 1300, device.width / 2, 300, 2000);
+    sleep(randomTime);
+}
